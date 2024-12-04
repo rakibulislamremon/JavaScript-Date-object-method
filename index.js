7a6bbdf2c894969ab90a50d5477251b331d204ed
rakibulislamremon <mdrakibulislamremon@gmail.com>
2024-12-04 19:27:27 +0600

    added Date object method

---
+++ index.js
@@ -0,0 +1,35 @@
+//getdate method
+/*var Object=new Date();
+console.log(
+    Object.getDate()
+)*/
+
+//getday method
+/*var obj= new Date();
+console.log(
+    obj.getDay()
+)*/
+
+// Get full year method
+/*var obj= new Date();
+console.log(
+    obj.getFullYear()
+)*/
+
+//get hours method\
+/*var obj= new Date();
+console.log(
+    obj.getHours()
+)*/
+
+//get second method
+/*var obj= new Date();
+console.log(
+    obj.getSeconds()
+)*/
+
+//get months method
+var obj= new Date();
+console.log(
+    obj.getMonth()
+)
